218967da8424293a7d0f59e3bf0f0e99c1b563fa
ebru <ebrualzey3@gmail.com>
2024-06-27 16:40:42 +0200

    first commit

--- src/test/java/pages/RolesPage.java
+++ src/test/java/pages/RolesPage.java
@@ -1,4 +1,10 @@
 package pages;
 
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
 public class RolesPage {
+    public RolesPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
 }
