a555ae8a9d2760f4a9bf83145e7b58ac489c7990
ercan <etogrul02@gmail.com>
2024-06-27 14:52:43 +0200

    first commit

--- src/test/java/pages/DepartmentPage.java
+++ src/test/java/pages/DepartmentPage.java
@@ -1,4 +1,15 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
 public class DepartmentPage {
+    public DepartmentPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(id = "firstName")
+    public WebElement firstName;
 }
