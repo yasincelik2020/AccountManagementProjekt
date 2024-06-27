a483febb77e4cefb8cae3fdba477a07b5884656e
rasit <sivaslirasit@gmail.com>
2024-06-27 16:23:42 +0200

    ilk veri

--- src/test/java/pages/UsersModulePage.java
+++ src/test/java/pages/UsersModulePage.java
@@ -1,4 +1,9 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class UsersModulePage {
+    @FindBy(xpath = "//a[.='Login']")
+    public WebElement LoginButton;
 }
